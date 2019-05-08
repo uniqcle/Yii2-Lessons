0821051ad4d5e37af303b6160598e27943658200
uniqcle <uniqcle@yandex.ru>
2019-05-08 10:57:00 +0700

    Redis Yii2

--- frontend/controllers/SaleController.php
+++ frontend/controllers/SaleController.php
@@ -1,7 +1,6 @@
 <?php
 namespace frontend\controllers;
 
-
 use yii\web\Controller;
 use frontend\models\Sale;
 use yii\helpers\ArrayHelper;
@@ -9,11 +8,18 @@ use yii\helpers\ArrayHelper;
 class SaleController extends Controller
 {
     public function actionIndex(){
-        $sale = ArrayHelper::getColumn( Sale::find()->select([ 'date', 'value'])->asArray()->all(), 'value' );
 
-        array_walk($sale, function(&$item){
-            $item = floatval($item);
-        });
+        $sale = \Yii::$app->cache->get('sale'); //Получаем кэш
+        if(!$sale) {  //Если кэша нет, то получаем данные
+            $sale = ArrayHelper::getColumn(
+                Sale::find()->select(['date', 'value'])->asArray()->all(), 'value'
+            );
+
+            array_walk($sale, function (&$item) {
+                $item = floatval($item);
+            });
+         \Yii::$app->cache->set('sale', $sale, 60*2);  //Устанавливаем кэш. 3-й параметр время
+        }
 
         return $this->render('index', [
             'sale' => $sale
